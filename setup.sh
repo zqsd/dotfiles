72073576972cbe9317cf62839230d88a412fe540
Clément Léger <wiz@haxx.es>
2014-01-31 17:32:24 +0100

    Vim needed plugins

--- setup.sh
+++ setup.sh
@@ -1,11 +1,7 @@
 #!/bin/sh
 
-stow bash
-mkdir -p ~/.bash
-if [ ! -d "~/.bash/liquidprompt" ]; then git clone https://github.com/nojhan/liquidprompt.git ~/.bash/liquidprompt ; fi
-
-stow git
-
-stow vim
-if [ ! -d "~/.vim/bundle/vundle" ]; then git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle ; fi
-echo "[vim] Todo: :BundleInstall"
+for i in bash git vim jshint
+do
+    echo "stow $i"
+    stow $i
+done
